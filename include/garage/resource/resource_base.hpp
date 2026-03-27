7789e56c06929fc03df7e22d834d3213fd208aaa
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-03-27 18:56:56 +0900

    Update resource_base.hpp

--- include/garage/resource/resource_base.hpp
+++ include/garage/resource/resource_base.hpp
@@ -11,6 +11,13 @@ namespace Garage
 	public:
 		virtual ~ResourceBase();
 
+		/**
+		 * @brief 해당 리소스를 불러옵니다.
+		 *
+		 * @param path 불러올 리소스의 경로
+         *
+         * @return bool 해당 리소스의 불러오기 성공 여부
+		 */
 		virtual bool Load(Path& path) = 0;
 	};
 }
