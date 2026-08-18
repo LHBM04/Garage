fc3e0103f7ff8118cb020916af2bbf8a2f74e93b
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-08-18 11:22:06 +0900

    서브시스템 추가

---
+++ include/garage/subsystem.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Garage
+{
+    /**
+     * @class Subsystem
+     * @brief 라이브러리 내 서브시스템을 정의합니다.
+     */
+    class Subsystem
+    {
+    public:
+        virtual ~Subsystem() noexcept = default;
+    };
+}
