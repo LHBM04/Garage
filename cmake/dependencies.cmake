2a2ce00a6f810c2818bf28411d61b0b5ba804733
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-09-15 12:56:08 +0900

    SDL3 종속성 추가

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,9 @@
+include (FetchContent)
+
+FetchContent_Declare(
+    sdl3
+    GIT_REPOSITORY https://github.com/libsdl-org/SDL
+    GIT_TAG        release-3.4.16
+)
+
+FetchContent_MakeAvailable(sdl3)
